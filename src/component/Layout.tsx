ca05f8b5acb9628490c7912910b4751a05209bb3
PC <nhanlt21.cdsg@saigontech.edu.vn>
2023-09-28 21:53:08 +0700

    adminPage

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -6,8 +6,12 @@ import Home from "./../component/Home";
 import Header from "../sanpham/Header";
 import Cart from "../cart/Cart";
 import Single_product from "../sanpham/Single_product";
-import { Provider } from 'react-redux';
-import store from '../reducer/store';
+import { Provider } from "react-redux";
+import store from "../reducer/store";
+import AdminLogin from "../admin/login";
+import AdminPage from "../admin/dashboard";
+import AddProductPage from "../admin/add";
+import EidtProductPage from "../admin/edit";
 const Layout = () => {
   return (
     <Provider store={store}>
@@ -16,6 +20,10 @@ const Layout = () => {
         <Route path="/Header" element={<Header />} />
         <Route path="/Cart" element={<Cart />} />
         <Route path="/Single_product" element={<Single_product />} />
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<AdminPage />} />
+        <Route path="/add" element={<AddProductPage />} />
+        <Route path="/edit/:id" element={<EidtProductPage />} />
       </Routes>
     </Provider>
   );
